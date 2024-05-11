f0545ffd110843be275fb8c34f7107a417b177ba
“Aisha <aisha-shafqat@hotmail.com>
2024-05-11 21:05:36 +0200

    Added comments in files

--- tests.sh
+++ tests.sh
@@ -1,10 +1,13 @@
-# Start the test sbuild process
+# Start the test build process
 echo '==> Starting tests..'
 
+# Run the prediction test
 echo '-- Running Prediction test ..'
 python3 src/models/predict_model.py
 
+# Run unit tests
 echo '-- Running unit tests..'
 pytest
 
+# Finish the test build process
 echo '==> Finished tests!'
